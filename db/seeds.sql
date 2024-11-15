1b824240dd4cf31b20f1f725205a10f4363cc066
Kevin Noguera <noguera.ke@gmail.com>
2024-11-14 22:51:23 -0500

    Add seed data for department, roles, and employee tables

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,13 +3,13 @@ INSERT INTO department (name) VALUES
 ('Photography'),
 ('Design');
 
-INSERT INTO roles (job_title, salary, department_id) VALUES
+INSERT INTO roles (title, salary, department_id) VALUES
 ('Director', 1000000, 1),
 ('Photographer', 75000, 2),
 ('Graphic Designer', 55000, 3);
 
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
-('Steven', 'Lawrence', 1, NULL),
-('Lucy', 'Niland', 02, 02),
-('Paul', 'Michaels', 03, 03);
+('Steven', 'Lawrence', 1, 01),
+('Lucy', 'Niland', 2, 02),
+('Paul', 'Michaels', 3, 03);
